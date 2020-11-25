1aefb3f4f52351e48de402230ee5bdf0adbdbe36
Paulina Carrillo <paulinacarri4@gmail.com>
2020-11-25 03:55:37 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -14,8 +14,12 @@ def loop_message_n_times(message, limit)
   end
 end
 
-def output_array
-  
+def output_array(array)
+  count = 0 
+  while count < array.length do
+    puts array[count]
+    count += 1 
+  end
 end
 
 def return_string_array
